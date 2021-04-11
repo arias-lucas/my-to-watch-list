5aa1ca4de3d9c4c5b83f34614cb4bc6845fa7931
Lucas Arias <arias.lucas.e@gmail.com>
2021-04-11 17:45:33 -0300

    Added theme names

--- src/themes.js
+++ src/themes.js
@@ -3,11 +3,13 @@ import React from "react";
 export const DARK_THEME = {
   backgroundColor: "rgb(27 26 26)",
   color: "#ded7d7",
+  name: "dark",
 };
 
 export const LIGHT_THEME = {
   backgroundColor: "lightgrey",
   color: "black",
+  name: "light",
 };
 
 export const ThemeContext = React.createContext({
